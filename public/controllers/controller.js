82a93733e72164541ed61f5decb33597b7b23486
coderamitamar <amitamar813@gmail.com>
2015-12-14 23:39:11 +0530

    complete

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,11 +2,40 @@ var myApp=angular.module('myApp',[]);
 myApp.controller('AppCtrl',['$scope','$http',
 	function ($scope,$http){
 		console.log("Hello world");
-		$http.get('/contacts').success(function (response){
-			$scope.contactlist=response;
-		});
+		var refresh=function(){
+			$http.get('/contacts').success(function (response){
+				$scope.contactlist=response;
+				$scope.contact="";
+			});
+		};
+		refresh();
 		$scope.addContact=function (){
 			console.log($scope.contact);
+			$http.post('/contacts',$scope.contact).success(function (response){
+				console.log(response);
+				refresh();
+			});
+		};
+		$scope.removeContact=function (id){
+			console.log(id);
+			$http.delete('/contacts/'+id).success(function (response){
+				refresh();
+			});
+		};
+		$scope.editContact=function (id){
+			console.log(id);
+			$http.get('/contacts/'+id).success(function (response){
+				$scope.contact=response;
+			});
+		};
+		$scope.updateContact=function (){
+			console.log($scope.contact);
+			$http.put('/contacts/'+$scope.contact._id,$scope.contact).success(function (response){
+				refresh();
+			});
+		};
+		$scope.clear=function (){
+			$scope.contact="";
 		};
 	}
 ]);
--- server.js
+++ server.js
@@ -3,12 +3,43 @@ var port=3000;
 var app=express();
 var mongojs=require('mongojs');
 var db=mongojs('contactlist',['contacts']);
+var bodyParser=require('body-parser');
 
 app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
 app.get('/contacts',function (req,res){
-	db.contactlist.find(function (err,docs){
+	db.contacts.find(function (err,docs){
 		res.json(docs);
 	});
+});
+app.post('/contacts',function (req,res){
+	console.log(req.body);
+	db.contacts.insert(req.body,function (err,docs){
+		res.json(docs);
+	});
+});
+app.delete('/contacts/:id',function (req,res){
+		var id=req.params.id;
+		console.log(id);
+		db.contacts.remove({_id:mongojs.ObjectId(id)},function (err,docs){
+			res.json(docs);
+		});
+});
+app.get('/contacts/:id',function (req,res){
+	var id=req.params.id;
+	console.log(id);
+	db.contacts.findOne({_id:mongojs.ObjectId(id)},function (err,docs){
+		res.json(docs);
+	});
+});
+app.put('/contacts/:id',function (req,res){
+	var id=req.params.id;
+	db.contacts.findAndModify({Query: {_id:mongojs.ObjectId(id)},
+		update: {$set: {name:req.body.name,email:req.body.email,mob:req.body.mob}},
+		new: true},function (err,docs){
+			res.json(docs);
+		}
+	);
 });
 app.listen(port);
 console.log('listening at '+port);

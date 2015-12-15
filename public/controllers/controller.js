5eeebdd43dbf7b70c35e1d18d02ccb51cb53e5dc
coderamitamar <amitamar813@gmail.com>
2015-12-15 12:22:38 +0530

    latest

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,7 +1,6 @@
 var myApp=angular.module('myApp',[]);
 myApp.controller('AppCtrl',['$scope','$http',
 	function ($scope,$http){
-		console.log("Hello world");
 		var refresh=function(){
 			$http.get('/contacts').success(function (response){
 				$scope.contactlist=response;
@@ -10,29 +9,28 @@ myApp.controller('AppCtrl',['$scope','$http',
 		};
 		refresh();
 		$scope.addContact=function (){
-			console.log($scope.contact);
-			$http.post('/contacts',$scope.contact).success(function (response){
-				console.log(response);
-				refresh();
-			});
+			if($scope.contact.name!=undefined && $scope.contact.name!=""){
+				$http.post('/contacts',$scope.contact).success(function (response){
+					refresh();
+				});
+			}
 		};
 		$scope.removeContact=function (id){
-			console.log(id);
 			$http.delete('/contacts/'+id).success(function (response){
 				refresh();
 			});
 		};
 		$scope.editContact=function (id){
-			console.log(id);
 			$http.get('/contacts/'+id).success(function (response){
 				$scope.contact=response;
 			});
 		};
 		$scope.updateContact=function (){
-			console.log($scope.contact);
-			$http.put('/contacts/'+$scope.contact._id,$scope.contact).success(function (response){
-				refresh();
-			});
+			if($scope.contact._id!=undefined){
+				$http.put('/contacts/'+$scope.contact._id,$scope.contact).success(function (response){
+					refresh();
+				});
+			}
 		};
 		$scope.clear=function (){
 			$scope.contact="";
